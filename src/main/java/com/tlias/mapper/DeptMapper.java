0a327e5acb06e017e70b3c038beda7efea023582
Masion-coder <2408766306@qq.com>
2025-03-04 20:45:59 +0800

    列表查询-结果封装

--- src/main/java/com/tlias/mapper/DeptMapper.java
+++ src/main/java/com/tlias/mapper/DeptMapper.java
@@ -12,6 +12,14 @@ public interface DeptMapper {
     /*
      * 查询所有部门
      */
+    // 方式一：使用@Results注解
+    // @Results({
+    //     @Result(column = "create_time", property = "createTime"),
+    //     @Result(column = "update_time", property = "updateTime")
+    // })
+    // 方式二：起别名
+    // @Select("select id, name, create_time createTime, update_time updateTime from dept order by update_time desc")
+    // 方式三：开启驼峰下划线自动映射
     @Select("select id, name, create_time, update_time from dept order by update_time desc")
     List<Dept> findAll();
 }
