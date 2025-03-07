86b87fe3f141b039dc32f5934f12ca92ca97e89f
Masion-coder <2408766306@qq.com>
2025-03-07 12:12:41 +0800

    Logback配置文件&日志级别

--- src/main/java/com/tlias/controller/DeptController.java
+++ src/main/java/com/tlias/controller/DeptController.java
@@ -11,49 +11,59 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tlias.module.Dept;
 import com.tlias.module.Result;
 import com.tlias.service.DeptService;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
-
+@Slf4j
 @RequestMapping("/depts")
 @RestController
 public class DeptController {
+    // private static final Logger log = LoggerFactory.getLogger(DeptController.class); // 固定的
+
     @Autowired
     private DeptService deptService;
     
     @GetMapping
     public Result findAll() {
-        System.out.println("查询全部部门数据");
+        // System.out.println("查询全部部门数据");
+        log.info("查询全部部门数据");
         List<Dept> deptList =  deptService.findAll();
         return Result.success(deptList);
     }
 
     @DeleteMapping
     public Result delete(Integer id) {
-        System.out.println("删除部门:" + id);
+        // System.out.println("删除部门:" + id);
+        log.info("删除部门:{}", id);
         deptService.deleteById(id);
         return Result.success();
     }
 
     @PostMapping
     public Result add(@RequestBody Dept dept) {
-        System.out.println("添加部门:" + dept);
+        // System.out.println("添加部门:" + dept);
+        log.info("添加部门:{}", dept);
         deptService.add(dept);
         return Result.success();
     }
     
     @GetMapping("/{id}")
     public Result getInfo(@PathVariable Integer id) {
-        System.out.println("根据id查询部门:" + id);
+        // System.out.println("根据id查询部门:" + id);
+        log.info("根据id查询部门:{}", id);
         return Result.success(deptService.getById(id));
     }
     
     @PutMapping
     public Result update(@RequestBody Dept dept) {
-        System.out.println("更新部门:" + dept);
+        // System.out.println("更新部门:" + dept);
+        log.info("更新部门:{}", dept);
         deptService.update(dept);
         return Result.success();
     }
--- src/test/java/com/tlias/LogTest.java
+++ src/test/java/com/tlias/LogTest.java
@@ -8,18 +8,20 @@ import org.slf4j.LoggerFactory;
 
 public class LogTest {
     private static final Logger log = LoggerFactory.getLogger(LogTest.class);
+
     @Test
     public void testLog() {
-        // System.out.println(LocalDateTime.now() + ":开始i计算...");
         log.debug(LocalDateTime.now() + ":开始i计算...");
         int sum = 0;
         int[] nums = { 1, 5, 3, 2, 1, 4, 5, 6, 7, 4, 34, 2, 23 };
         for (int num : nums) {
             sum += num;
-            // System.out.println("i计算结果为：" + sum);
-            // System.out.println(LocalDateTime.now() + "结束i计算...");
-            log.info("i计算结果为：" + sum);
-            log.debug(LocalDateTime.now() + "结束i计算...");
         }
+        log.info("i计算结果为：" + sum);
+        log.debug(LocalDateTime.now() + "结束i计算...");
+
+        log.trace("trace...");
+        log.warn("warn...");
+        log.error("error...");
     }
 }
