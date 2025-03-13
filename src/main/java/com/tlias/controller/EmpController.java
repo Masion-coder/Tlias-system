675a375005d59c50beef9b26a014ed7cb55ed20c
Masion-coder <2408766306@qq.com>
2025-03-13 21:12:25 +0800

    员工管理-删除员工-请求参数接收

--- src/main/java/com/tlias/controller/EmpController.java
+++ src/main/java/com/tlias/controller/EmpController.java
@@ -1,10 +1,14 @@
 package com.tlias.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tlias.model.Emp;
@@ -40,6 +44,24 @@ public class EmpController {
     public Result save(@RequestBody Emp emp) {
         log.info("新增员工");
         empService.save(emp);
+        return Result.success();
+    }
+
+    /*
+     * 删除员工 - 数组接收
+     */
+    // @DeleteMapping
+    // public Result delete(Integer[] ids) {
+    //     log.info("根据id批量删除员工:{}", Arrays.toString(ids));
+    //     return Result.success();
+    // }
+    
+    /*
+     * 删除员工 - list集合接收(要加@RequestParam注解)
+     */
+    @DeleteMapping
+    public Result delete(@RequestParam List<Integer> ids) {
+        log.info("根据id批量删除员工:{}", ids.toString());
         return Result.success();
     }
 }
